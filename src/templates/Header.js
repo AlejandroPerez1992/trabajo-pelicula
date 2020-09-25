9dbc1b89e08234a1c15e289997e1b4de6e108985
AlejandroPerez1992 <69736400+AlejandroPerez1992@users.noreply.github.com>
2020-09-25 17:26:59 -0300

    travis3

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,14 +1,13 @@
 
 const Header = () => {
     const view = `
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <div class="fixed-top">
       <nav class="navbar navbar-expand-xl navbar-dark mx-background-top-linear">
         <div class="container">
-          <a class="navbar-brand" rel="nofollow" href="/" ;">
+          <a class="navbar-brand" rel="nofollow" href="/">
             <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg">
               <strong>www.themoviedb.org</strong>    
           </a>
